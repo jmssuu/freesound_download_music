18288f3d4bb90e8d7ae43445b782409520e199c5
jmssuu <happy23056@yahoo.com.tw>
2021-01-10 18:19:13 +0800

    first_code

--- freesound_dwonload_music.py
+++ freesound_dwonload_music.py
@@ -5,7 +5,7 @@
 # 2. login freesound()
 # 3. Download freesound music
 ################################ 
-# python執行引述輸入格式說明 [檔名.py] [tag or search] [搜尋文字] [起始下載數量(從1開始數)] [結尾下載數量(含最後一個)] [下載位置(最後面必須加'/')]
+# python執行引述輸入格式說明 [檔名.py] [tag or search] [搜尋文字] [起始下載數量(從1開始數)] [結尾下載數量(含最後一個)] [下載位置(最後面必須加'/')] 
 ################################
 import requests
 from bs4 import BeautifulSoup
